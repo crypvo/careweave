0588caf68fba9be3bfe95f890a702e328016f2a9
crypvo-zono <64460607+crypvo-zono@users.noreply.github.com>
2025-08-26 16:17:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-// vite.config.ts（置き換え）
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  root: './src',
   build: {
-    outDir: 'dist',
-    ssr: false,        // ← SSRを明示的に無効化
+    outDir: '../dist',
   }
 })
